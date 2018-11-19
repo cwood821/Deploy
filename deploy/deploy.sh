08b2c7b19a358abb546c7dee50c8be82f222e301
Christian Wood <christian@christianwood.net>
2018-11-18 20:41:38 -0600

    Add post-deploy hook, more robust checks for failure

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,7 +7,7 @@ FAILURE_CODE=1
 
 # Check if we have the proper configuration via a .env
 if [ ! -f .env ]; then
-    echo "❌ Could not find the .env file. Launch aborted."
+    echo "❌ Could not find a .env file. Launch aborted."
     exit 1
 fi
 
@@ -15,22 +15,29 @@ fi
 source .env
 
 # Setup timestamp log file for this deployment
-NOW=$(date +'%m-%d-%Y-%H-%M')
+NOW=$(date +'%m-%d-%Y-%H-%M-%S')
 DEPLOYMENT_LOG_FILE=$DEPLOYMENT_LOG_DIRECTORY"/"$NOW-deployment-log.txt
 
-# Check if we have the deployment log directory
-if [ ! -d "$DEPLOYMENT_LOG_DIRECTORY" ]; then
+# Check if we have the deployment log directory, and that it has a name given in the 
+# the configuration
+if [ ! -d "$DEPLOYMENT_LOG_DIRECTORY" ] && [ ! -z $DEPLOYMENT_LOG_DIRECTORY ]; then
   mkdir -pv $1 $DEPLOYMENT_LOG_DIRECTORY
-fi
+elif [ -z $DEPLOYMENT_LOG_DIRECTORY ]; then
+  echo "No name given for deployment log directory."
+  exit $FAILURE_CODE
+fi 
 
-# Pre-deployment hook script: exists, executable
-if [ -f $PRE_DEPLOYMENT_SCRIPT ] && [ -x $PRE_DEPLOYMENT_SCRIPT ]; then 
+# Pre-deployment hook script: has value, exists, executable
+if [ ! -z $PRE_DEPLOYMENT_SCRIPT ] && [ -f $PRE_DEPLOYMENT_SCRIPT ] && [ -x $PRE_DEPLOYMENT_SCRIPT ]; then 
+  # Todo add time to monitor how long predeploy script takes
   ./$PRE_DEPLOYMENT_SCRIPT >> $DEPLOYMENT_LOG_FILE
   # Check that the pre-deployment hook script exited succesfully
   if [ "$?" -ne "0" ]; then
     echo "Failure on the launch pad."
     exit $FAILURE_CODE
   fi
+else 
+  echo "No pre-deployment script found or it is not executable." >> $DEPLOYMENT_LOG_FILE
 fi
 
 # Rsync new files and delete removed files
@@ -51,8 +58,20 @@ if [[ $HTTP_RESPONSE == $HTTP_OK_RESPONSE ]]; then
   echo "Launched! 🚀"
 else
   echo "Houston, we have a problem."
-  echo "HTTP $HTTP_RESPONSE response from $WEB_URL";
+  echo "HTTP $HTTP_RESPONSE response from $WEB_URL" | tee -a $DEPLOYMENT_LOG_FILE;
   exit $FAILURE_CODE
+fi
+
+# Post-deployment hook script: has value, exists, executable
+if [ ! -z $POST_DEPLOYMENT_SCRIPT ] && [ -f $POST_DEPLOYMENT_SCRIPT ] && [ -x $POST_DEPLOYMENT_SCRIPT ]; then 
+  ./$POST_DEPLOYMENT_SCRIPT >> $DEPLOYMENT_LOG_FILE
+  # Check that the post-deployment hook script exited succesfully
+  if [ "$?" -ne "0" ]; then
+    echo "Failed in post-deployment script" | tee -a $DEPLOYMENT_LOG_FILE;
+    exit $FAILURE_CODE
+  fi
+else 
+  echo "No post-deployment script found or it is not executable." >> $DEPLOYMENT_LOG_FILE
 fi
 
 # Complete succesfully
